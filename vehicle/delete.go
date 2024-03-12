bd242e85e38e34271510f3b0edbb34670c8003cd
Claire Guerrini <claire.guerrini2@hotmail.fr>
2024-03-12 14:40:42 +0000

    delete fixed

--- vehicle/delete.go
+++ vehicle/delete.go
@@ -2,7 +2,9 @@ package vehicle
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/outmrhoust/vehicle-server/pkg/httputil"
 	"github.com/outmrhoust/vehicle-server/storage"
 	"go.uber.org/zap"
 )
@@ -20,5 +22,37 @@ func NewDeleteHandler(store storage.Store, logger *zap.Logger) *DeleteHandler {
 }
 
 func (d *DeleteHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	http.Error(rw, "Not Implemented", http.StatusInternalServerError)
+	// http.Error(rw, "Not Implemented", http.StatusInternalServerError)
+
+	id := r.PathValue("id")
+	id64, err := strconv.ParseInt(id, 10, 64)
+
+	if err != nil {
+		d.logger.Error(
+			"ParseInt error",
+			zap.Error(err),
+		)
+		httputil.ServeError(rw, http.StatusInternalServerError, err)
+		return
+	}
+
+	vehicleDeleted, err := d.store.Vehicle().Delete(r.Context(), id64)
+
+	if err != nil {
+		d.logger.Error(
+			"Could not delete the vehicle",
+			zap.Error(err),
+		)
+		httputil.ServeError(rw, http.StatusInternalServerError, err)
+		return
+	}
+
+	if vehicleDeleted == true {
+		rw.WriteHeader(http.StatusNoContent)
+		return
+	} else {
+		rw.WriteHeader(404)
+		return
+	}
+
 }
